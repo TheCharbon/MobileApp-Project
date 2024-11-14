894238ba541d6cd43ffded2935aae438f986862e
TheCharbon <Colin.Madore@stu.crc-lennox.qc.ca>
2024-11-14 15:45:01 -0500

    trying to test navigation but gradle file is not cooperating

--- app/src/main/java/com/example/mobileapp_project/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp_project/MainActivity.kt
@@ -3,27 +3,25 @@ package com.example.mobileapp_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.mobileapp_project.ui.theme.MobileAppProjectTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MobileAppProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") { HomeView(navController) }
+                    composable("analytics") { AnalyticsView(navController) }
+                    composable("entry") { EntryView(navController) }
                 }
             }
         }
@@ -31,17 +29,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeView(navController: NavController){
+    Column {
+        Row {
+            Navbar(navController)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    MobileAppProjectTheme {
-        Greeting("Android")
+fun AnalyticsView(navController: NavController){
+    Column {
+        Row {
+            Navbar(navController)
+        }
+    }
+}
+
+@Composable
+fun EntryView(navController: NavController){
+    Column {
+        Row {
+            Navbar(navController)
+        }
     }
 }
