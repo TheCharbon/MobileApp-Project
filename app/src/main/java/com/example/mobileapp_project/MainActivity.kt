2650f360a697727e0eaae6241abd14c720f10fd0
TheCharbon <Colin.Madore@stu.crc-lennox.qc.ca>
2024-11-21 15:38:33 -0500

    home page complete

--- app/src/main/java/com/example/mobileapp_project/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp_project/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.mobileapp_project
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Label
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -33,37 +41,114 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Home page
+ *
+ * Shows the home page,
+ * has access to analytics and entry pages
+ */
 @Composable
-fun HomeView(navController: NavController, modifier: Modifier = Modifier){
-    Column (
-        modifier
+fun HomeView(navController: NavController){
+    Column(
+        modifier = Modifier
             .fillMaxSize()
             .padding(top = 16.dp)
     ){
-        Row (
-            modifier
-
-        ){
+        //navbar
+        Row{
             Navbar(navController)
         }
-        Text(text = "Email")
+
+        //title
+        TitleLabel(title = "Welcome to the finance tracker app!")
+
+        //buttons
+        Row(
+            modifier = Modifier
+                .padding(20.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            //analytics
+            Button(onClick = {
+                navController.navigate("analytics")
+            }){
+                Text(text = "View Analytics")
+            }
+
+            //entry
+            Button(onClick = {
+                navController.navigate("entry")
+            }){
+                Text(text = "Add new record")
+            }
+        }
+
     }
 }
 
+/**
+ * Analytics page
+ *
+ * Shows the analytics gotten from
+ * the database and processes it into a graph
+ */
 @Composable
 fun AnalyticsView(navController: NavController){
-    Column {
-        Row {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
+    ){
+        //navbar
+        Row{
             Navbar(navController)
         }
+
+        //title
+        TitleLabel(title = "Analytics")
+
+        //graph, etc...
     }
 }
 
+/**
+ * Entry page
+ *
+ * Show the entry fields necessary to input a new
+ * record into the database.
+ */
 @Composable
 fun EntryView(navController: NavController){
-    Column {
-        Row {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp)
+    ){
+        //navbar
+        Row{
             Navbar(navController)
         }
+
+        //title
+        TitleLabel(title = "New Record")
+
+        //text fields
+    }
+}
+
+/**
+ * Title composable
+ * 
+ * This creates the title label for each page
+ */
+@Composable
+fun TitleLabel(title: String){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(text = title)
     }
 }
